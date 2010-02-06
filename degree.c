b4deec3b676669eaefe596df56344f9fe928c40e
Lorenzo Fundaró García <lorenzo@ac.labf.usb.ve>
2010-02-05 22:10:35 -0430

    se modificaron argumentos de funcion compare, se implementa paso uno del algoritmo de dsatur

--- degree.c
+++ degree.c
@@ -1,19 +1,20 @@
+#include <stddef.h>
 #include "main.h"
+
 
 /* Funcion que dado un arreglo de lista de adyacencias */
 /* y el número de vértices del grafo, retorna un arreglo */
 /* de tuplas vértice-grado */
 
-tuple[] degree(linked_list main_col[], int vertex_num) {
-  tuple deg_vert[vertex_num]; //Arreglo de elementos vertice-grado
+tuple * degree(linked_list main_col[], int vertex_num, tuple deg_vert[]) {
   int i;
   int count = 0;
   linked_list * aux;
-  for(i = 0, i < vextex_num; i++) {
-      aux = main_col[i].next; // Apunta al primer adyacente
+  for(i = 0; i < vertex_num; i++) {
+    aux = (linked_list *) main_col[i].next; // Apunta al primer adyacente
       while (aux != NULL) {
         count++;
-        aux = aux->next;
+        aux = (linked_list *) aux->next;
       }
       // Se asigna una tuple vertice-grado al arreglo deg_vert
       tuple new_tuple = { i, count };
@@ -26,7 +27,9 @@ tuple[] degree(linked_list main_col[], int vertex_num) {
 /* Funcion que compara dos elementos tuple segun los  */
 /* grados de los vértices. */
 
-int compare(const tuple * a, const tuple * b) {
+int compare(const void * __a, const void * __b) {
+  tuple * a = (tuple *) __a;
+  tuple * b = (tuple *) __b;
   if (a->degree < b->degree) 
     return 1;
   if (a->degree > b->degree)
--- dsatur.c
+++ dsatur.c
@@ -1,3 +1,11 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include "degree.c"
+
 int dsatur(linked_list main_col[], tuple deg_vert[]) {
-  return 1;
+  // Ordenamiento decreciente de grados de vertices
+  tuple * base = &deg_vert[0];  // Apuntador a primer elemento
+  size_t nmemb = sizeof deg_vert / sizeof deg_vert[0]; // Número de elementos
+  size_t size = sizeof deg_vert[0]; // Tamaño de un elemento
+  qsort(base, nmemb, size, compare);
 }
